a3614aec9b4627aa073b7fea1c086c4ab781dbb9
Shyla27 <sheilabernicew@gmail.com>
2020-01-22 17:23:10 +0300

    connected to firebase and finished up on authentication

--- app/src/main/java/com/example/joblink/Home.java
+++ app/src/main/java/com/example/joblink/Home.java
@@ -7,14 +7,24 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class Home extends AppCompatActivity {
 
     private Button mLogout;
+    @BindView(R.id.intern) Button mIntern;
+    @BindView(R.id.company) Button mCompany;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+
+        mCompany.setOnClickListener((View.OnClickListener) this);
+        mIntern.setOnClickListener((View.OnClickListener) this);
+
         mLogout = findViewById(R.id.button);
         mLogout.setOnClickListener(new View.OnClickListener() {
             @Override
